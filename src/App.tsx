a067d3eac7e2f39091fd521bf8e175a417d70fb2
Nando Erni <nando.erni@outlook.de>
2025-09-05 13:11:01 +0200

    nice header :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { useState, useEffect } from "react";
-import gamesData from "./data/games.json";
-import myGamesData from "./data/mygames.json";
+import gamesData from "@/data/games.json";
 import { ThemeProvider } from "@/components/theme-provider"
-import { ModeToggle } from "@/components/mode-toggle";
 import { Separator } from "@/components/ui/separator";
-import { Badge } from "@/components/ui/badge";
 import Games from "@/components/games";
-import exitLogo from "@/assets/EXIT-Logo.png";
+import Navbar from "@/components/navbar";
 
 export default function App() {
   const [games, setGames] = useState<typeof gamesData>([]);
@@ -18,18 +15,8 @@ export default function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="w-full flex flex-col items-center">
-        <div className="flex flex-col items-center my-5">
-          <img src={exitLogo} alt="EXIT Logo" className="p-4" />
-          <h1 className="text-sm font-light text-center mb-5">Personal Collection</h1>
-          <ModeToggle />
-        </div>
-
-
-        <div className="flex flex-wrap justify-center items-center space-x-4">
-              <Badge className="mb-6">Total Games: {games.length}</Badge>
-              <Badge className="mb-6">Games played: {Object.keys(myGamesData.gameIDsPlayed).length}</Badge>
-              <Badge className="mb-6">Games owned: {myGamesData.gameIDsOwned.length}</Badge>
-        </div>
+        
+        <Navbar />
         
         <Separator className="mb-6" />
         <Games games={games} />
---
+++ src/components/navbar.tsx
@@ -0,0 +1,60 @@
+import gamesData from "@/data/games.json";
+import myGamesData from "@/data/mygames.json";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Badge } from "@/components/ui/badge";
+import exitLogo from "@/assets/EXIT-Logo.png";
+import { useCallback, useEffect, useState } from "react";
+import { cn, debounce } from "@/lib/utils";
+
+export default function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = useCallback(
+    debounce(() => {
+      if (window.scrollY > 20 && !scrolled) {
+        setScrolled(true);
+      } else if (window.scrollY < 10 && scrolled) {
+        setScrolled(false);
+      }
+    }, 100),
+    [scrolled]
+  );
+
+  useEffect(() => {
+    window.addEventListener("wheel", handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener("wheel", handleScroll);
+    };
+  }, [handleScroll]);
+
+  return (
+    <header
+      className={cn(
+        "sticky top-0 z-50 w-full border-b backdrop-blur scroll-smooth transition-all duration-300",
+        scrolled ? "py-2" : "py-4"
+      )}
+    >
+      <div className={cn("absolute top-4 right-4")}>
+        <ModeToggle />
+      </div>
+      <p
+        className={cn(
+          "font-light text-center transition-all duration-300",
+          scrolled ? "text-3xl m-5" : "text-5xl m-10"
+        )}
+      >
+        EXIT Personal Collection
+      </p>
+
+      <div className="flex flex-wrap justify-center items-center gap-2">
+        <Badge className="mb-6">Total Games: {gamesData.length}</Badge>
+        <Badge className="mb-6">
+          Games played: {Object.keys(myGamesData.gameIDsPlayed).length}
+        </Badge>
+        <Badge className="mb-6">
+          Games owned: {myGamesData.gameIDsOwned.length}
+        </Badge>
+      </div>
+    </header>
+  );
+}
